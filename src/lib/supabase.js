40d917e922fa4fb8597fa3f9738f28260a328edb
roberttedt-jr <roberttedt@gmail.com>
2026-02-21 00:15:02 +0100

    Eliminando archivos de la app antigua

--- src/lib/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://gfxzsyqcdriojozxfwgt.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmeHpzeXFjZHJpb2pvenhmd2d0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4MzQxNzksImV4cCI6MjA3OTQxMDE3OX0.DJPZ7sV19w_4x5jFhoEVcpft-rPC6bnCJ_B80dayWV4';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Flicker/', // <--- CAMBIA ESTO POR EL NOMBRE DE TU REPO EN GITHUB
-})
